a9afebb0d3bd7aabb43be18f56e5123db85c890b
hmtmcse <hmtm.cse@gmail.com>
2021-02-10 15:59:11 +0600

    marshmallow with orm

---
+++ flask-marshmallow-sqlalchemy.py
@@ -0,0 +1,141 @@
+import datetime
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from marshmallow import Schema, fields, ValidationError, pre_load
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/quotes.db"
+db = SQLAlchemy(app)
+
+##### MODELS #####
+
+
+class Author(db.Model):  # type: ignore
+    id = db.Column(db.Integer, primary_key=True)
+    first = db.Column(db.String(80))
+    last = db.Column(db.String(80))
+
+
+class Quote(db.Model):  # type: ignore
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
+    author = db.relationship("Author", backref=db.backref("quotes", lazy="dynamic"))
+    posted_at = db.Column(db.DateTime)
+
+
+##### SCHEMAS #####
+
+
+class AuthorSchema(Schema):
+    id = fields.Int(dump_only=True)
+    first = fields.Str()
+    last = fields.Str()
+    formatted_name = fields.Method("format_name", dump_only=True)
+
+    def format_name(self, author):
+        return "{}, {}".format(author.last, author.first)
+
+
+# Custom validator
+def must_not_be_blank(data):
+    if not data:
+        raise ValidationError("Data not provided.")
+
+
+class QuoteSchema(Schema):
+    id = fields.Int(dump_only=True)
+    author = fields.Nested(AuthorSchema, validate=must_not_be_blank)
+    content = fields.Str(required=True, validate=must_not_be_blank)
+    posted_at = fields.DateTime(dump_only=True)
+
+    # Allow client to pass author's full name in request body
+    # e.g. {"author': 'Tim Peters"} rather than {"first": "Tim", "last": "Peters"}
+    @pre_load
+    def process_author(self, data, **kwargs):
+        author_name = data.get("author")
+        if author_name:
+            first, last = author_name.split(" ")
+            author_dict = dict(first=first, last=last)
+        else:
+            author_dict = {}
+        data["author"] = author_dict
+        return data
+
+
+author_schema = AuthorSchema()
+authors_schema = AuthorSchema(many=True)
+quote_schema = QuoteSchema()
+quotes_schema = QuoteSchema(many=True, only=("id", "content"))
+
+##### API #####
+
+
+@app.route("/authors")
+def get_authors():
+    authors = Author.query.all()
+    # Serialize the queryset
+    result = authors_schema.dump(authors)
+    return {"authors": result}
+
+
+@app.route("/authors/<int:pk>")
+def get_author(pk):
+    try:
+        author = Author.query.get(pk)
+    except IntegrityError:
+        return {"message": "Author could not be found."}, 400
+    author_result = author_schema.dump(author)
+    quotes_result = quotes_schema.dump(author.quotes.all())
+    return {"author": author_result, "quotes": quotes_result}
+
+
+@app.route("/quotes/", methods=["GET"])
+def get_quotes():
+    quotes = Quote.query.all()
+    result = quotes_schema.dump(quotes, many=True)
+    return {"quotes": result}
+
+
+@app.route("/quotes/<int:pk>")
+def get_quote(pk):
+    try:
+        quote = Quote.query.get(pk)
+    except IntegrityError:
+        return {"message": "Quote could not be found."}, 400
+    result = quote_schema.dump(quote)
+    return {"quote": result}
+
+
+@app.route("/quotes/", methods=["POST"])
+def new_quote():
+    json_data = request.get_json()
+    if not json_data:
+        return {"message": "No input data provided"}, 400
+    # Validate and deserialize input
+    try:
+        data = quote_schema.load(json_data)
+    except ValidationError as err:
+        return err.messages, 422
+    first, last = data["author"]["first"], data["author"]["last"]
+    author = Author.query.filter_by(first=first, last=last).first()
+    if author is None:
+        # Create a new author
+        author = Author(first=first, last=last)
+        db.session.add(author)
+    # Create new quote
+    quote = Quote(
+        content=data["content"], author=author, posted_at=datetime.datetime.utcnow()
+    )
+    db.session.add(quote)
+    db.session.commit()
+    result = quote_schema.dump(Quote.query.get(quote.id))
+    return {"message": "Created new quote.", "quote": result}
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True, port=5000)
---
+++ fms.py
@@ -0,0 +1,65 @@
+import os
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import ValidationError, pprint, fields
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+from sqlalchemy.orm import session
+
+db = SQLAlchemy()
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(BASE_DIR, 'fms.sqlite3')
+
+# Register ORM
+db.init_app(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    age = db.Column(db.Float())
+
+
+with app.app_context():
+    db.create_all()
+
+
+class UserDTO(SQLAlchemySchema):
+    class Meta:
+        model = User
+        load_instance = True
+
+    name = auto_field()
+    email = fields.Email(required=True, error_messages={"required": "Please enter email.", "invalid": "Invalid email address."})
+    age = auto_field()
+
+
+@app.route("/user/create", methods=['POST'])
+def create_user():
+    try:
+        user = UserDTO().load(request.json, session=session)
+        db.session.add(user)
+        db.session.commit()
+        return {"data": {"status": "success", "message": "Validation Success"}}
+    except ValidationError as err:
+        errors = {}
+        if err and err.messages:
+            for message in err.messages:
+                error_text = ""
+                for text in err.messages[message]:
+                    error_text += text
+                errors[message] = error_text
+        pprint(err.messages)
+    return {"data": {"status": "error", "errors": errors}}
+
+
+@app.route('/')
+def bismillah():
+    return "FMS Project"
+
+
+if __name__ == '__main__':
+    app.run()
