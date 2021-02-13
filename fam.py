0f2369d2a2824caadf68f925450ec31bc23746b2
hmtmcse <hmtm.cse@gmail.com>
2021-02-13 07:25:15 +0600

    marshmallow apispec

--- fam.py
+++ fam.py
@@ -1,6 +1,5 @@
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
-from apispec_webframeworks.flask import FlaskPlugin
 from flask import Flask, request
 from marshmallow import Schema, fields, ValidationError, pprint
 
@@ -11,7 +10,7 @@ spec = APISpec(
     title="Swagger Petstore",
     version="1.0.0",
     openapi_version="3.0.2",
-    plugins=[ MarshmallowPlugin()],
+    plugins=[MarshmallowPlugin()],
 )
 
 jwt_scheme = {"type": "http", "scheme": "bearer", "bearerFormat": "JWT"}
@@ -20,22 +19,131 @@ spec.components.security_scheme("jwt", jwt_scheme)
 
 class UserDTO(Schema):
     name = fields.String(required=True, error_messages={"required": "Please enter name."})
-    email = fields.Email(required=True, error_messages={"required": "Please enter email.", "invalid": "Invalid email address."})
+    email = fields.Email(required=True, error_messages={
+        "required": "Please enter email.", "invalid": "Invalid email address."})
     age = fields.Float()
 
 
+class MessageDTO(Schema):
+    message = fields.String()
+    code = fields.Integer()
+
+
 spec.components.schema("User", schema=UserDTO)
+spec.components.schema("Message", schema=MessageDTO)
+
+# Create
 spec.path(
-    path="/gist/{gist_id}",
+    path="/user/create",
     operations=dict(
-        get=dict(
-            responses={"200": {"content": {"application/json": {"schema": "User"}}, "description": "kisu akta"}}
+        post=dict(
+            requestBody={"required": True, "content": {"application/json": {"schema": "User"}}},
+            responses={"200": {"content": {"application/json": {"schema": {
+                "anyOf": [{"$ref": "#/components/schemas/User"}, {"$ref": "#/components/schemas/Message"}]}}},
+                "description": "Description"}}
+        )
+    )
+)
+
+# Update
+spec.path(
+    path="/user/update/{id}",
+    operations=dict(
+        post=dict(
+            requestBody={"required": True, "content": {"application/json": {"schema": "User"}}},
+            responses={"200": {"content": {"application/json": {"schema": {
+                "anyOf": [{"$ref": "#/components/schemas/User"}, {"$ref": "#/components/schemas/Message"}]}}},
+                "description": "Description"}}
         )
     ),
     parameters=[
         {
-            "name": "gist_id",
-            "in": "path"
+            "name": "id",
+            "in": "path",
+            "schema": {
+                "type": "integer"
+            }
+        }
+    ]
+)
+
+# List with pagination
+spec.path(
+    path="/user/list",
+    operations=dict(
+        get=dict(
+            responses={"200": {"content": {"application/json": {"schema": {
+                "type": "array",
+                "items": "User",
+            }}},
+                "description": "Description"}}
+        )
+    ),
+    parameters=[
+        {
+            "name": "offset",
+            "in": "query",
+            "schema": {
+                "type": "integer"
+            }
+        },
+        {
+            "name": "max",
+            "in": "query",
+            "schema": {
+                "type": "integer"
+            }
+        },
+        {
+            "name": "order-by",
+            "in": "query",
+            "schema": {
+                "type": "string"
+            }
+        },
+        {
+            "name": "order-field",
+            "in": "query",
+            "schema": {
+                "type": "string"
+            }
+        }
+    ]
+)
+
+# Details by Id
+spec.path(
+    path="/user/details/{id}",
+    operations=dict(
+        get=dict(
+            responses={"200": {"content": {"application/json": {"schema": "User"}}, "description": "Details"}}
+        )
+    ),
+    parameters=[
+        {
+            "name": "id",
+            "in": "path",
+            "schema": {
+                "type": "integer"
+            }
+        }
+    ]
+)
+# Delete
+spec.path(
+    path="/user/delete/{id}",
+    operations=dict(
+        delete=dict(
+            responses={"200": {"content": {"application/json": {"schema": "Message"}}, "description": "Details"}}
+        )
+    ),
+    parameters=[
+        {
+            "name": "id",
+            "in": "path",
+            "schema": {
+                "type": "integer"
+            }
         }
     ]
 )
