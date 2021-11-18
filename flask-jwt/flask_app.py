b3dd28b0f283ada7e1ea81f2627678e6ce4ce6a8
hmtmcse <hmtm.cse@gmail.com>
2021-11-18 20:37:48 +0600

    relationship

--- flask-jwt/flask_app.py
+++ flask-jwt/flask_app.py
@@ -10,8 +10,13 @@ def bismillah():
 @app.before_request
 def before_request():
     print("before_request")
-    return "Access Denied"
+    # return "Access Denied"
+
+
+def br():
+    print("BR")
 
 
 if __name__ == '__main__':
+    app.before_request_funcs.setdefault(None, []).append(br)
     app.run()
