fe2c0569847f80b5c6eb8e3afb20cf8fbd49918d
hmtmcse <hmtm.cse@gmail.com>
2022-10-15 22:09:08 +0600

    test

--- logical/list_experiment.py
+++ logical/list_experiment.py
@@ -1,3 +1,5 @@
+import re
+
 list1 = ['A', 'B', 'C', 'D', 'E', 'F']
 
 print(list1)
@@ -9,3 +11,17 @@ print(list1)
 list1.pop(6)
 
 print(list1)
+
+insert_at = 0
+
+if insert_at is not None:
+    print("Yessssssssssss")
+
+# data = "/api/user/name"
+data = "/"
+
+print(data.startswith('/api'))
+
+# prefix = "/api"
+# x = re.search('^' + prefix, data)
+# print(x)
