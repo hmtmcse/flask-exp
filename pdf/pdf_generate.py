2c7679f1b90f1354dff01a27fad583ed815af88c
Touhid Mia <touhid@banglafighter.com>
2024-04-18 08:37:18 +0600

    Working On PDF

--- pdf/pdf_generate.py
+++ pdf/pdf_generate.py
@@ -3,9 +3,11 @@ from pdf.pweb_pdf import PWebPDF
 # PWebPDF.generate(url="https://www.hmtmcse.com", pdf_store_path="pdf-from-url.pdf")
 # PWebPDF.generate(html_file_path="html-css/ticket.html", pdf_store_path="pdf-from-html-css.pdf", embed_css=True)
 PWebPDF.generate(
+    resource_root_path="html-css",
     html_content='''
     <h1>PWeb PDF</h1>
     <p>Content goes here</p>
+    <img src="logo.png">
 ''',
     css='@page { size: A3; margin: 1cm } h1{color: red;}',
     pdf_store_path="pdf-from-html-css-str.pdf",
--- pdf/pweb_pdf.py
+++ pdf/pweb_pdf.py
@@ -6,18 +6,19 @@ class PWebPDF:
 
     @staticmethod
     def generate(
-                 html_content: str = None,
-                 html_file_path: str = None,
-                 pdf_store_path: str = None,
-                 url: str = None,
-                 css: str = None,
-                 css_file_path: str = None,
-                 css_url: str = None,
-                 encoding=None,
-                 content_zoom: float = 1,
-                 font_config=FontConfiguration(),
-                 embed_css=False,
-                 media_type='print'):
+            html_content: str = None,
+            html_file_path: str = None,
+            pdf_store_path: str = None,
+            url: str = None,
+            css: str = None,
+            css_file_path: str = None,
+            css_url: str = None,
+            resource_root_path: str = None,
+            encoding=None,
+            content_zoom: float = 1,
+            font_config=FontConfiguration(),
+            embed_css=False,
+            media_type='print'):
 
         # Started Main Coding
         if not html_content and not html_file_path and not url:
@@ -25,6 +26,7 @@ class PWebPDF:
 
         # Configuring HTML Content
         html_object = HTML(
+            base_url=resource_root_path,
             string=html_content,
             filename=html_file_path,
             url=url,
@@ -38,6 +40,7 @@ class PWebPDF:
         if css_url or css or css_file_path:
             css_font_conf = font_config
             stylesheets = [CSS(
+                base_url=resource_root_path,
                 string=css,
                 filename=css_file_path,
                 url=css_url,
